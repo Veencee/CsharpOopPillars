4d7d7507d45c7f48a4572f3745adf654d6852f0b
Veencee <110591295+Veencee@users.noreply.github.com>
2022-08-11 10:15:04 -0700

    Add files via upload

--- SOlid/Program.cs
+++ SOlid/Program.cs
@@ -1,21 +1,32 @@
-﻿namespace SROCprinciples
+﻿namespace SortArray
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Army army1 = new Army();
-            Airforce airforce1 = new Airforce();
-            Navy navy1 = new Navy();
+            int[] num = new int[] { 21, 34, 16, 54, 43, 42, 31, 26, 73, 43 };
+            int temp ;
 
-            double y = 10000;
-            //Console.ReadLine(x);
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = i + 1; j < 10; j++)
+                {
+                    if (num[i] < num[j])
+                    {
+                        temp = num[i];
+                        num[i] = num[j];
+                        num[j] = temp;
+                    }
+                }
+                //Console.WriteLine(String.Join(" ", num));
+                //Console.WriteLine(num);
 
-            Console.WriteLine(army1.Reward("Major", y));
-            
-            Console.WriteLine(airforce1.Reward("Air ViceMarshall", y));
-            Console.WriteLine(navy1.Reward("Commodore", y));
-
+            }
+            Console.WriteLine("Array in Descending order: ");
+            foreach (int i in num) 
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
